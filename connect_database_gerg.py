77f1d7154ca1faf5f814ecc6df476246f27a4848
leecyg1 <gregleecy@yahoo.com.sg>
2020-02-29 14:04:03 +0800

    gerg commit db

---
+++ connect_database_gerg.py
@@ -0,0 +1,20 @@
+
+# connecting to mysql database to the python code, from a remote computer
+
+import mysql.connector
+db_connection = mysql.connector.connect(user='greg', password='sharedpassword',host='172.22.143.201',
+                                     port='3306', database='bf3210_database')
+db_cursor = db_connection.cursor()  # create database cursor to perform SQL operations
+
+SQL = "SHOW TABLES"
+db_cursor.execute(SQL)
+for table in db_cursor:
+    print(table)
+
+SQL = "SELECT * FROM macro"
+db_cursor.execute(SQL)
+for each in db_cursor:
+    print(each)
+
+# close connection to mysql database
+db_connection.close()
